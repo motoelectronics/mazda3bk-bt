5052d527392bea4d245a223967c5f6e9131424eb
MotoElectronics <ao.yurlov[at]yandex.ru>
2025-08-01 00:13:51 +0700

    added some fix

--- mazda3bk-bt.ino
+++ mazda3bk-bt.ino
@@ -8,45 +8,45 @@
   и управляет цифровыми выходами согласно заданным условиям.
   
   Условия:
-  - Если напряжение 3.70-3.90В держится  >300мс -> включаем MUTE_PIN  (кнопка MUTE CSR8645 BT module)
-  - Если напряжение 3.70-3.90В держится >3000мс -> включаем RESET_PIN (reset CSR8645 BT module)
-  - Если напряжение 2.30-2.50В держится  >300мс -> включаем PREV_PIN  (кнопка PREV CSR8645 BT module)
-  - Если напряжение 1.50-1.80В держится  >300мс -> включаем NEXT_PIN  (кнопка NEXT CSR8645 BT module)
-  - При старте системы на RESET_PIN подаётся импульс 3000мс (reset CSR8645 BT module)
+  - Если напряжение 3.85-4.05В держится >3000мс -> включаем POWER_EN_PIN (reset CSR8645 BT module)
+  - Если напряжение 2.52-2.72В держится  >300мс -> включаем PREV_PIN  (кнопка PREV CSR8645 BT module)
+  - Если напряжение 1.78-1.98В держится  >300мс -> включаем NEXT_PIN  (кнопка NEXT CSR8645 BT module)
+  - При старте системы на POWER_EN_PIN подаётся импульс 3000мс (reset CSR8645 BT module)
 
   Замеры (DEBUG 1, J701 ST-SW1 белый с черной полосой):
   - No   = 4.38В
-  - Mute = 3.80В
-  - Next = 1.67В
-  - Prev = 2.40В
+  - Mute = 3.95В //Check
+  - Next = 1.88В //Check
+  - Prev = 2.62В //Check
   - Vol+ = 0.98В
   - Vol- = 0.41В
   - Mode = 3.13В
 */
 
 #define DEBUG 0
+#define NUM_READINGS 5 // Количество измерений для усреднения
 
 // ========== ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ И КОНСТАНТЫ ==========
 
 // Пороговые значения напряжения (в вольтах)
-const float VOLTAGE_LOW_1  = 3.70;  // Нижний порог 1-го диапазона
-const float VOLTAGE_HIGH_1 = 3.90;  // Верхний порог 1-го диапазона
-const float VOLTAGE_LOW_2  = 2.30;  // Нижний порог 2-го диапазона
-const float VOLTAGE_HIGH_2 = 2.50;  // Верхний порог 2-го диапазона
-const float VOLTAGE_LOW_3  = 1.50;  // Нижний порог 3-го диапазона
-const float VOLTAGE_HIGH_3 = 1.80;  // Верхний порог 3-го диапазона
+const float VOLTAGE_LOW_1  = 3.85;  // Нижний порог 1-го диапазона
+const float VOLTAGE_HIGH_1 = 4.05;  // Верхний порог 1-го диапазона
+const float VOLTAGE_LOW_2  = 2.52;  // Нижний порог 2-го диапазона
+const float VOLTAGE_HIGH_2 = 2.72;  // Верхний порог 2-го диапазона
+const float VOLTAGE_LOW_3  = 1.78;  // Нижний порог 3-го диапазона
+const float VOLTAGE_HIGH_3 = 1.98;  // Верхний порог 3-го диапазона
 
 // Временные параметры (в миллисекундах)
-const unsigned long DURATION_THRESHOLD_1 = 300;  // Минимальное время удержания напряжения
-const unsigned long DURATION_THRESHOLD_2 = 3000; // Длительное время удержания напряжения MUTE для сброса BT модуля
-const unsigned long INITIAL_DELAY        = 3000; // Длительность импульса на D4 при старте
+const unsigned long DURATION_THRESHOLD_1 = 3000; // Длительное время удержания напряжения MUTE для сброса BT модуля
+const unsigned long DURATION_THRESHOLD_2 = 300;  // Минимальное время удержания напряжения
+const unsigned long INITIAL_DELAY        = 5000; // Длительность импульса на POWER_EN_PIN при старте
 
 // Назначение пинов Arduino
-const int SIGNAL_PIN = A0;  // Аналоговый вход для измерения напряжения
-const int MUTE_PIN   = 3;   // Цифровой выход 1 для кнопки MUTE
-const int PREV_PIN   = 4;   // Цифровой выход 2 для кнопки PREV
-const int NEXT_PIN   = 5;   // Цифровой выход 3 для кнопки NEXT
-const int RESET_PIN  = 6;  // Цифровой выход 4 для сброса BT модуля
+const int BUTTONS_PIN    = A5;  // Аналоговый вход для измерения напряжения
+const int START_STOP_PIN = 5;   // Цифровой выход 1 для кнопки MUTE
+const int PREV_PIN       = 4;   // Цифровой выход 2 для кнопки PREV
+const int NEXT_PIN       = 3;   // Цифровой выход 3 для кнопки NEXT
+const int POWER_EN_PIN   = 6;   // Цифровой выход 4 для сброса BT модуля
 
 // Переменные для отслеживания времени удержания напряжения
 unsigned long startTime1 = 0;  // Таймер для 1-го диапазона
@@ -59,8 +59,7 @@ bool condition2Active = false;  // Флаг активности 2-го диап
 bool condition3Active = false;  // Флаг активности 3-го диапазона
 
 // Настройки фильтра
-const int numReadings = 10;   // Количество измерений для усреднения
-float readings[numReadings];  // Массив для хранения измерений
+float readings[NUM_READINGS]; // Массив для хранения измерений
 int readIndex = 0;            // Текущий индекс в массиве
 float filteredVoltage = 0;    // Отфильтрованное напряжение
 
@@ -70,13 +69,13 @@ void setup() {
   analogReference(DEFAULT);
 
   // Настройка цифровых пинов как выходов
-  pinMode(MUTE_PIN,  OUTPUT);
+  pinMode(START_STOP_PIN,  OUTPUT);
   pinMode(PREV_PIN,  OUTPUT);
   pinMode(NEXT_PIN,  OUTPUT);
-  pinMode(RESET_PIN, OUTPUT);
+  pinMode(POWER_EN_PIN, OUTPUT);
   
   // Инициализация массива нулями
-  for (int i = 0; i < numReadings; i++) {
+  for (int i = 0; i < NUM_READINGS; i++) {
     readings[i] = 0;
   }
 
@@ -88,10 +87,10 @@ void setup() {
   Serial.println("BT module is resetting...");
 #endif
   
-  // При старте подаём импульс на RESET_PIN длительностью 3000 мс
-  digitalWrite(RESET_PIN, HIGH);
+  // При старте подаём импульс на POWER_EN_PIN длительностью INITIAL_DELAY
+  digitalWrite(POWER_EN_PIN, LOW);
   delay(INITIAL_DELAY);
-  digitalWrite(RESET_PIN, LOW);
+  digitalWrite(POWER_EN_PIN, HIGH);
 
 #if DEBUG
   // Вывод в монитор порта для отладки
@@ -110,22 +109,22 @@ void loop() {
   checkCondition3();  // Проверка NEXT
   
   // Небольшая задержка для стабильности системы
-  delay(20);
+  delay(10);
 }
 
 // ========== ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ==========
 void readVoltage()
 {
   // Считываем новое значение и добавляем в массив
-  readings[readIndex] = (float) (analogRead(SIGNAL_PIN) * 5.0) / 1024;
-  readIndex = (readIndex + 1) % numReadings;
+  readings[readIndex] = (float) (analogRead(BUTTONS_PIN) * 5.0) / 1024;
+  readIndex = (readIndex + 1) % NUM_READINGS;
 
   // Вычисляем среднее значение напряжения
   filteredVoltage = 0;
-  for (int i = 0; i < numReadings; i++) {
+  for (int i = 0; i < NUM_READINGS; i++) {
     filteredVoltage += readings[i];
   }
-  filteredVoltage /= numReadings;
+  filteredVoltage /= NUM_READINGS;
 
 #if DEBUG
   // Вывод в монитор порта для отладки
@@ -144,32 +143,25 @@ void checkCondition1() {
         // Первое попадание в диапазон - запоминаем время
         startTime1 = millis();
 #if DEBUG
-        Serial.println("Starting point for MUTE_PIN");
+        Serial.println("Starting point for POWER_EN_PIN");
 #endif
       } else if (millis() - startTime1 >= DURATION_THRESHOLD_1) {
-        // Условие держится дольше 300 мс - активируем выход
-        condition1Active = true;
-        digitalWrite(MUTE_PIN, HIGH);
-#if DEBUG
-        Serial.println("Output MUTE_PIN is high");
-#endif
-      } else if (millis() - startTime1 >= DURATION_THRESHOLD_2) {
         // Условие держится дольше 3000 мс - активируем выход
         condition1Active = true;
-        digitalWrite(RESET_PIN, HIGH);
+        digitalWrite(POWER_EN_PIN, LOW);
 #if DEBUG
-        Serial.println("Output RESET_PIN is high");
+        Serial.println("Output POWER_EN_PIN is low");
 #endif
+        delay(DURATION_THRESHOLD_2);
       }
     }
   } else {
     // Напряжение вышло из диапазона
     if (condition1Active) {
       // Если выход был активен - выключаем его
-      digitalWrite(MUTE_PIN,  LOW);
-      digitalWrite(RESET_PIN, LOW);
+      digitalWrite(POWER_EN_PIN, HIGH);
 #if DEBUG
-      Serial.println("Output MUTE_PIN/RESET_PIN is low");
+      Serial.println("Output POWER_EN_PIN is high");
 #endif
     }
     // Сбрасываем флаги и таймер
@@ -187,12 +179,13 @@ void checkCondition2() {
 #if DEBUG
         Serial.println("Starting point for PREV_PIN");
 #endif
-      } else if (millis() - startTime2 >= DURATION_THRESHOLD_1) {
+      } else if (millis() - startTime2 >= DURATION_THRESHOLD_2) {
         condition2Active = true;
         digitalWrite(PREV_PIN, HIGH);
 #if DEBUG
         Serial.println("Output PREV_PIN is high");
 #endif
+        delay(DURATION_THRESHOLD_2);
       }
     }
   } else {
@@ -216,12 +209,13 @@ void checkCondition3() {
 #if DEBUG
         Serial.println("Starting point for NEXT_PIN");
 #endif
-      } else if (millis() - startTime3 >= DURATION_THRESHOLD_1) {
+      } else if (millis() - startTime3 >= DURATION_THRESHOLD_2) {
         condition3Active = true;
         digitalWrite(NEXT_PIN, HIGH);
 #if DEBUG
         Serial.println("Output NEXT_PIN is high");
 #endif
+        delay(DURATION_THRESHOLD_2);
       }
     }
   } else {
